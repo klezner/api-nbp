8e2425d6a748bc12fbb845d4838dff96fe01750b
Krzysztof Lezner <krzys3ek@gmail.com>
2020-07-04 12:02:05 +0200

    Loading currency is done

--- src/main/java/pl/kl/apinbp/Main.java
+++ src/main/java/pl/kl/apinbp/Main.java
@@ -1,7 +1,24 @@
 package pl.kl.apinbp;
 
+import java.util.Optional;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        NBPApiParameters parameters = new NBPApiParameters();
+
+        loadAndSetCurrency(scanner, parameters);
+    }
+
+    private static void loadAndSetCurrency(Scanner scanner, NBPApiParameters parameters) {
+        do {
+            System.out.println("Please enter currency [dolar, euro, rubel]:");
+            Optional<NBPCurrency> optionalNBPCurrency = NBPCurrency.parse(scanner.nextLine());
+            if (optionalNBPCurrency.isPresent()) {
+                parameters.setCurrency(optionalNBPCurrency.get());
+            }
+        } while (parameters.getCurrency() == null);
     }
 }
---
+++ src/main/java/pl/kl/apinbp/NBPCurrency.java
@@ -0,0 +1,34 @@
+package pl.kl.apinbp;
+
+import java.util.Optional;
+
+public enum NBPCurrency {
+    AMERICAN_DOLLAR("USD"),
+    RUSSIAN_RUBEL("RUB"),
+    EURO("EUR");
+
+    private String shortName;
+
+    NBPCurrency(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    //NBPCurrency currency = NBPCurrency.EURO;
+    //sout(currency); EURO
+    //sout(currency.getShortName); EUR
+
+    public static Optional<NBPCurrency> parse(String input) {
+        if (input.toUpperCase().contains("DOLAR")) {
+            return Optional.of(NBPCurrency.AMERICAN_DOLLAR);
+        } else if (input.toUpperCase().contains("RUBEL")) {
+            return Optional.of(NBPCurrency.RUSSIAN_RUBEL);
+        } else if (input.toUpperCase().contains("EURO")) {
+            return Optional.of(NBPCurrency.EURO);
+        }
+        return Optional.empty();
+    }
+}
